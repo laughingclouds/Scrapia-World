58f0a49f53318054c59240ffd687aa8de8e75324
r3a10god <zenithtaiga@gmail.com>
2021-05-17 01:05:47 +0530

    Added a method to login and go to a desired chapter through click implementation

--- scrapia_world.py
+++ scrapia_world.py
@@ -1,5 +1,8 @@
 # scrapia_world = Scrape wuxia world...
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Firefox
 from time import sleep      # for timeouts, cuz' you don't wanna get your IP banned...
 from env_db import *
@@ -15,12 +18,14 @@ def install_addon_clean_tabs_get_login_window(driver) -> None:
     and also return focus to the login window.
     """
 
-    driver.install_addon('/opt/WebDriver/fox_ext/touch_vpn_secure_vpn_proxy_for_unlimited_access-4.2.1-fx.xpi')
+    # driver.install_addon('/opt/WebDriver/fox_ext/touch_vpn_secure_vpn_proxy_for_unlimited_access-4.2.1-fx.xpi')
+    # Don't need the vpn if we're gonna with the click implementation
     driver.install_addon('/opt/WebDriver/fox_ext/ghostery_privacy_ad_blocker-8.5.5-an+fx.xpi')
     driver.install_addon('/opt/WebDriver/fox_ext/privacy_badger-2021.2.2-an+fx.xpi')
     driver.get("https://www.wuxiaworld.com/account/login")
+    WebDriverWait(driver, 7).until(EC.presence_of_all_elements_located((By.TAG_NAME, "form")))
 
-    sleep(20)        # please setup touch vpn within this time...hopefully it works...
+    driver.implicitly_wait(20)      # please setup touch vpn within this time...hopefully it works...
 
 
     MAIN_HANDLE: str = driver.current_window_handle     # Points to ww /account/login/
@@ -59,6 +64,28 @@ install_addon_clean_tabs_get_login_window(driver)
 login_key_strokes(driver)
 # This makes us a "regular reader" (a reader that is registered) (made up term) (since unregistered users are called
 # "guest readers")
+# Until here, all we do is login, now we're going to the novel page
+
+# New stuff below
+driver.get("https://www.wuxiaworld.com/novel/against-the-gods/")
+
+driver.find_element_by_partial_link_text("Chapters").click()
+
+# This will open all the panels in the chapters section of ww /atg
+# starts from '2' because in the initial setup the first panel is open by default, clicking on it, will close
+# and hence, hide the chapters withing that panel.
+try:
+    for volume_no in range(2, 19):
+        driver.find_element_by_partial_link_text(f"Volume {volume_no} ").click()
+except Exception as e:
+    print(e)
+finally:    # To do, add this stuff in a function or make it work as a method
+    pass
+
+# Now we go to the relevant chapter or `CH_NO`
+driver.find_element_by_partial_link_text("Chapter " + str(CH_NO) + " ").click()
+# BASE_URL[26: ] = /novel/against-...
+# New stuff above
 
 try:
     while CH_NO < LATEST_CH_NO:
@@ -67,7 +94,6 @@ try:
         # optional, you could add a line to stop execution when a certain `CH_NO` has been scraped.
 
     print("All present chapters scraped...\nEnding...")
-    end_cleanly()
 except KeyboardInterrupt:
     # end_cleanly()
     pass
@@ -76,3 +102,5 @@ except Exception as e:
     print(e)
     # end_cleanly()
     pass
+finally:
+    end_cleanly()
