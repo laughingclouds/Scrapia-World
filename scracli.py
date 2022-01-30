7d3d6c8c6be145ec747c5c851572e99e9bdd94a2
laughingclouds <zenithtaiga@gmail.com>
2022-01-30 18:05:09 +0530

    1) styling 2) adding a new option to the cli shell command, now you open in either headless mode or graphic mode headless by default graphic mode on invoking the option

--- scracli.py
+++ scracli.py
@@ -1,4 +1,5 @@
 from cmd import Cmd
+from email.policy import default
 from json import load
 from os import system as systemCmd, listdir
 from tkinter import EXCEPTION
@@ -15,8 +16,10 @@ def cli(ctx):
     """Command line tool for working with downloaded content"""
     pass
 
+
 class OverrideDefault(scrapia_shell.ScrapiaShell):
     """Overrides `default` function of `ScrapiaShell`"""
+
     # We will not define a __init__ because we want to use
     # the base classes' __init__
 
@@ -34,33 +37,38 @@ def _get_chapter_number_list(chapter_list: list[str]) -> list[int]:
 
     def _get_number_from_string(string_: str) -> int:
         """A function to get a number from a string, for example, getting a chapter number from the chapter title."""
-        number_as_str: str = ''
+        number_as_str: str = ""
         was_prev_element_digit: bool = False
         for element in string_:
             if element.isdigit():
                 number_as_str += element
                 was_prev_element_digit = True
-            elif not(element.isdigit()) and was_prev_element_digit:
+            elif not (element.isdigit()) and was_prev_element_digit:
                 return int(number_as_str)
             else:
                 continue
         return int(number_as_str)
 
-    sorted_chapter_list: list[int] = [_get_number_from_string(chapter_title) for chapter_title in chapter_list]
+    sorted_chapter_list: list[int] = [
+        _get_number_from_string(chapter_title) for chapter_title in chapter_list
+    ]
     sorted_chapter_list.sort()
     return sorted_chapter_list
+
 
 def _return_novel_info_dict(novel_name: str) -> dict[str, str]:
     """Returns a dictionary with info on `novel_name`. Refer to the json file to what info is given."""
 
-    with open("novel_page_info.json", 'r') as novel_page_fobj:
+    with open("novel_page_info.json", "r") as novel_page_fobj:
         return load(novel_page_fobj)["novel_page_info"][novel_name]
+
 
 def _return_chapter_list(path: str) -> list[str]:
     return listdir(path)
 
+
 @cli.command()
-@click.argument('novel_name', type=str)
+@click.argument("novel_name", type=str)
 def count_words(novel_name: str) -> None:
     """Returns the word count of all the files within a directory. Takes in a list of filenames."""
     word_count: int = 0
@@ -68,26 +76,38 @@ def count_words(novel_name: str) -> None:
     BASE_PATH = novel_info_dict["NOVEL_SAVE_PATH_BASE"]  # Base path for the novel
 
     for filename in _return_chapter_list(BASE_PATH):
-        with open(f"{BASE_PATH}/{filename}", 'r') as file_object:
+        with open(f"{BASE_PATH}/{filename}", "r") as file_object:
             chapter_content = file_object.read()
             word_count += len(chapter_content.split())
-    
+
     click.echo(word_count)
 
 
 @cli.command()
-@click.option("--latest-chapter", is_flag=True, flag_value=True,
-    help="This will show the latest chapter number as well.")
-@click.option("--list-all-chapters", is_flag=True, flag_value=True,
-    help="This will return the list of all the chapters.")
+@click.option(
+    "--latest-chapter",
+    is_flag=True,
+    flag_value=True,
+    help="This will show the latest chapter number as well.",
+)
+@click.option(
+    "--list-all-chapters",
+    is_flag=True,
+    flag_value=True,
+    help="This will return the list of all the chapters.",
+)
 @click.argument("novel_name", type=str)
-def check(novel_name: str, latest_chapter: bool=False, list_all_chapters: bool=False):
+def check(
+    novel_name: str, latest_chapter: bool = False, list_all_chapters: bool = False
+):
     """Check wether any chapters are missing."""
 
     # This open the json file to return the path of the novel `novel_name`. Open the json to know its structure.
-    with open("novel_page_info.json", 'r') as novel_page_info_fobj:
-        novel_page_dict: dict[str, str] = load(novel_page_info_fobj)["novel_page_info"][novel_name]
-        chapter_list: list[str] = listdir(novel_page_dict['NOVEL_SAVE_PATH_BASE'])
+    with open("novel_page_info.json", "r") as novel_page_info_fobj:
+        novel_page_dict: dict[str, str] = load(novel_page_info_fobj)["novel_page_info"][
+            novel_name
+        ]
+        chapter_list: list[str] = listdir(novel_page_dict["NOVEL_SAVE_PATH_BASE"])
 
     sorted_chapter_list: list[int] = _get_chapter_number_list(chapter_list)
 
@@ -102,7 +122,9 @@ def check(novel_name: str, latest_chapter: bool=False, list_all_chapters: bool=F
     if not flag:
         click.echo("Everythings fine chap.")
     else:
-        click.echo(f"There seems to be a problem, please check if the following chapters exist:\n{possible_missing_chapters}")
+        click.echo(
+            f"There seems to be a problem, please check if the following chapters exist:\n{possible_missing_chapters}"
+        )
 
     if latest_chapter:
         click.echo(f"This is the latest chapter number: {sorted_chapter_list[-1]}")
@@ -113,14 +135,13 @@ def check(novel_name: str, latest_chapter: bool=False, list_all_chapters: bool=F
 @cli.command()
 def cls():
     """clear screen"""
-    try: 
+    try:
         if WIN:
             systemCmd("cls")
         else:
             systemCmd("clear")
     except EXCEPTION:
         click.clear()
-
 
 
 @cli.command()
@@ -131,17 +152,26 @@ def greet(n, name):
     for _ in range(n):
         click.echo(f"Hello {name}")
 
+
 @cli.command()
+@click.option(
+    "-headless",
+    default=1,
+    help="Start in headless mode. Default value is 1. "
+    + "Set to 0, to start in graphical mode.",
+)
 @click.argument("novel_name")
 @click.pass_context
-def shell(ctx, novel_name: str):
+def shell(ctx, headless: int, novel_name: str):
     """Start a scraping session in a shell"""
-    safe: str = "You are safe to go if you're in the terminal."
-    option = input(f"Warning, invoking this within the shell will restart it.{safe}\nContinue? (y/n): ")
-    if option == 'y':
-        OverrideDefault(novel_name, ctx).cmdloop()
+    safe = "You are safe to go if you're in the terminal."
+    warning = "Warning, invoking this within the shell will restart it."
+    option = input(f"{warning} {safe}\nContinue? (y/n): ")
+    if option == "y":
+        OverrideDefault(headless, novel_name, ctx).cmdloop()
     else:
         return None
+
 
 if __name__ == "__main__":
     cli()
